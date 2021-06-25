218365fda168de272e43d82cce01354c4bbdee2b
MasterTony04 <mastertony04@gmial.com>
2021-06-25 10:33:39 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+int bSwitch = 12, fSwitch = 11;
+int motA = A0, motB =A1 ;
+bool forwardMotor = true;
+void setup() {
+// write your initialization code here
+}
+
+void loop() {
+// write your code here
+}
