a7835544c5513dc35a4475d90b3e24fa3b3c2c7f
MasterTony04 <mastertony04@gmial.com>
2021-06-26 16:13:16 +0300

    third upload

--- src/motorController.h
+++ src/motorController.h
@@ -6,3 +6,35 @@
 #define MOTORCONTROLLER_MOTORCONTROLLER_H
 
 #endif //MOTORCONTROLLER_MOTORCONTROLLER_H
+
+
+//void sendWifiData() {
+//    Serial.println("+++++++++++++++++++++++++++++");
+//    DynamicJsonDocument doc(128);
+//    doc["temperature"] = temp;
+//    doc["humidity"] = hum;
+//    doc["incubationStatus"] = incubationActive;
+//    doc["powerPriority"] = powerPriority;
+//    doc["birdType"] = birdTypeNo;
+//    doc["incubationDays"] = incubationDays;
+//    doc["hatchingDays"] = hatchingDays;
+//    doc["numberOfDaysElapsed"] = day;
+//    serializeJson(doc, WiFiSerial);
+//
+////    serializeJson(doc, json);
+////    Serial.println(WiFiSerial.read());
+//
+//    serializeJson(doc, Serial);
+//    if (WiFiSerial.availableForWrite() > 0) {
+//        serializeJson(doc, WiFiSerial);
+//    }
+//    String status = "";
+//    status = WiFiSerial.readStringUntil('0');
+//    if (status.length() > 0) {
+//        Serial.println("Data sent successfully");
+//    } else {
+//        Serial.println("Data not sent");
+//    }
+//    Serial.println("=========================================");
+//    delay(10000);
+//}
